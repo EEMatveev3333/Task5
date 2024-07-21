be814f8b95d53601c09c1cb3845107c059349cdc
EEMatveev <eematveev333@gmail.com>
2024-07-21 13:49:09 +0300

    ver 0.9.8.1 (Full without tests, interface version)

--- src/test/java/TestServices.java
+++ src/test/java/TestServices.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.Test;
@@ -21,6 +22,7 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import java.util.Date;
 
+@ActiveProfiles("test")
 //@RunWith(SpringRunner.class)
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
